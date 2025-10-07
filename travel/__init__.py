1880aaf193037b811c2efd5eb3bb1563fd09fa52
Dylan Rabie <168161163+Doozie01@users.noreply.github.com>
2025-10-07 11:00:47 +0000

    Linked pages and flask template for Login and Register

--- travel/__init__.py
+++ travel/__init__.py
@@ -8,9 +8,6 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # A secret key for the session object
-    app.secret_key = 'secretkey'
-
     # DB configuration and initialisation
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
     db.init_app(app)
--- travel/views.py
+++ travel/views.py
@@ -26,3 +26,11 @@ def create_event():
 @bp.route('/bookings')
 def bookings():
     return render_template('bookings.html', active_page="bookings")
+
+@bp.route('/login')
+def login():
+    return render_template('login.html', active_page="login")
+
+@bp.route('/register')
+def register():
+    return render_template('register.html', active_page="register")
