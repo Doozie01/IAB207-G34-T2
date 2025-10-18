5704c04ca09107580c0d17173e24e7b66b2e35d9
Barn33y <bobisanawesomename123@gmail.com>
2025-10-18 03:42:20 +0000

    Login and Register Functionality with Database

--- travel/__init__.py
+++ travel/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .views import bp as main_bp
 from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -18,5 +19,14 @@ def create_app():
 
     from . import auth
     app.register_blueprint(auth.authbp)
+
+    login_manager = LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    from.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
--- travel/views.py
+++ travel/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, url_for, request, session
+from flask_login import login_required, current_user
 
 bp = Blueprint('main', __name__)
 
